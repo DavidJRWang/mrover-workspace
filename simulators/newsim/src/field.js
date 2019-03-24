76cf06606f9b6f112ace29cf52da9cbfe2398b25
David Wang <daviwang@umich.edu>
2019-03-24 13:25:58 -0400

    Restarted frontend because linking together .html files because the benefits of structure are not worth the effort.

---
+++ simulators/newsim/src/field.js
@@ -0,0 +1,6 @@
+var field = document.getElementById("field");
+var context = field.getContext("2d");
+context.font = "24px Verdana";
+content.fillStyle = "#444";
+context.textAlign = "center";
+contet.fillText("Heyyyy Baby", field.width/2, field.height/2);
